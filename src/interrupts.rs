848841a3b7111818d3919f7774ed3059c03c70cd
zqning <zqning@tencent.com>
2021-07-26 17:33:28 +0800

    paging

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -111,22 +111,6 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         }
     }
 
-    let key = match scancode {
-        0x02 => Some('1'),
-        0x03 => Some('2'),
-        0x04 => Some('3'),
-        0x05 => Some('4'),
-        0x06 => Some('5'),
-        0x07 => Some('6'),
-        0x08 => Some('7'),
-        0x09 => Some('8'),
-        0x0a => Some('9'),
-        0x0b => Some('0'),
-        _ => None,
-    };
-    if let Some(key) = key {
-        print!("{}", key);
-    }
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     };
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 use ning_os::hlt_loop;
+use ning_os::memory::BootInfoFrameAllocator;
 use ning_os::println;
 use ning_os::print;
 use x86_64::structures::paging::PageTable;
@@ -16,53 +17,9 @@ pub mod memory;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    /*
-    for i in 0..2000 {
-        println!("this is the {} row", i);
-    }
-    */
-    //panic!("fuck!");
     ning_os::init();
     println!("hello world");
-    //x86_64::instructions::interrupts::int3();
-    println!("it worked!");
     /*
-    fn stack_overflow() {
-        stack_overflow()
-    }
-    stack_overflow();
-    */
-    /*
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    }
-    */
-    /* 
-    loop{
-        for i in 0..1000 {
-        }
-            print!("-");
-    }
-    */
-    /* 
-    use x86_64::VirtAddr;
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe {actived_level_4_table(phys_mem_offset)};
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe {&mut *ptr};
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
-    }
-    */
     use x86_64::VirtAddr;
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mapper = unsafe {memory::init(phys_mem_offset)};
@@ -76,6 +33,19 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         let virt = VirtAddr::new(address);
         let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
+    }
+    */
+    use x86_64::{structures::paging::Page, VirtAddr};
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe{memory::init(phys_mem_offset)};
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe {
+        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
     }
     #[cfg(test)]
     test_main();
